5e916910fb02496cc55fd96446f324ea49f3d15f
saitsenel <saitsenel44@gmail.com>
2020-09-23 13:53:33 +0300

    Importing project

---
+++ KurumsalWeb/Controllers/AdminController.cs
@@ -0,0 +1,47 @@
+﻿using KurumsalWeb.Models;
+using KurumsalWeb.Models.DataContext;
+using KurumsalWeb.Models.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KurumsalWeb.Controllers
+{
+    public class AdminController : Controller
+    {
+        KurumsalDBContext db = new KurumsalDBContext();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            var sorgu = db.Kategori.ToList();
+            return View(sorgu);
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Admin admin)
+        {
+            var login = db.Admin.Where(x => x.Eposta == admin.Eposta).SingleOrDefault();
+            if (login.Eposta==admin.Eposta && login.Sifre==admin.Sifre)
+            {
+                Session["adminId"] = login.AdminId;
+                Session["eposta"] = login.Eposta;
+                return RedirectToAction("Index","Admin");
+            }
+            ViewBag.Uyari = "Kullanıcı adı ya da Şifre yanlış";
+            return View(admin);
+        }
+        public ActionResult Logout() 
+        {
+            Session["adminId"] = null;
+            Session["eposta"] = null;
+            Session.Abandon();
+            return RedirectToAction("Login", "Admin");
+        }
+    }
+    
+}
